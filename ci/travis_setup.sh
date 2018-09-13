7fc33b01bc81d33f0d869948f1430c8a196707ea
Jonathan Fine <finej@purdue.edu>
2018-09-13 12:01:53 -0400

    Additional CI

---
+++ ci/travis_setup.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Originally written for Chemfiles <https://github.com/chemfiles/chemfiles> by
+# Evan Nemerson.  For documentation, bug reports, support requests,
+# etc. please use <https://github.com/nemequ/icc-travis>.
+
+export CMAKE_ARGS="-DCMAKE_BUILD_TYPE=Debug -DINCHI_BUILD_SHARED=ON -DINCHI_BUILD_EXE=ON -DINCHI_DEMOS=ON -DINCHI_TESTING=ON -DRINCHI_TESTING=ON"
+
+if [[ "$EMSCRIPTEN" == "ON" ]]; then
+    # Install a Travis compatible emscripten SDK
+    wget https://github.com/chemfiles/emscripten-sdk/archive/master.tar.gz
+    tar xf master.tar.gz
+    ./emscripten-sdk-master/emsdk activate
+    source ./emscripten-sdk-master/emsdk_env.sh
+
+    export CMAKE_CONFIGURE='emcmake'
+    export CMAKE_ARGS="-DINCHI_TESTING=ON -DRINCHI_TESTING=ON -DTEST_RUNNER=node -DCMAKE_BUILD_TYPE=Release"
+
+    # Install a modern cmake
+    cd $HOME
+    wget https://cmake.org/files/v3.9/cmake-3.9.3-Linux-x86_64.tar.gz
+    tar xf cmake-3.9.3-Linux-x86_64.tar.gz
+    export PATH=$HOME/cmake-3.9.3-Linux-x86_64/bin:$PATH
+
+    export CC=emcc
+    export CXX=em++
+
+    return
+fi
+
+if [[ "$VALGRIND" == "ON" ]]; then
+    export CMAKE_ARGS="$CMAKE_ARGS -DTEST_RUNNER=valgrind"
+return
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    if [[ "$CC" == "gcc" ]]; then
+        export CC=gcc-5
+        export CXX=g++-5
+    fi
+fi
+
+if [[ "$USE_ICC" == "ON" ]]; then
+    /bin/sh $TRAVIS_BUILD_DIR/ci/install-icc.sh
+    source ~/.bashrc
+    export CC=icc
+    export CXX=icpc
+fi
+
+if [[ "$USE_PGI" == "ON" ]]; then
+    /bin/sh $TRAVIS_BUILD_DIR/ci/install-pgi.sh
+    export CC=pgcc
+    export CXX=pgc++
+fi
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    brew update
+    if [[ "$CC" == "gcc" ]]; then
+        brew install gcc@5
+        export CC=gcc-5
+        export CXX=g++-5
+    fi
+fi
+
+if [[ "$ARCH" == "x86" ]]; then
+    export CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_C_FLAGS=-m32"
+fi
--- inchi/InChITests.cmake
+++ inchi/InChITests.cmake
@@ -3,200 +3,59 @@ execute_process(
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
 )
 
-add_test(
-    NAME inchi_std_01
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-std-01.txt its-std-01.log NUL -AuxNone -NoLabels -Key
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
+if(${TEST_RUNNER} STREQUAL "valgrind")
+    set(
+        RUNNER_COMMAND
+        "valgrind" "--leak-check=full" "--dsymutil=yes" "--error-exitcode=125"
+    )
+elseif(${TEST_RUNNER} STREQUAL "wine")
+    set(RUNNER_COMMAND "wine")
+elseif(${TEST_RUNNER} STREQUAL "node")
+    set(RUNNER_COMMAND "node")
+else()
+    set(RUNNER_COMMAND "")
+endif()
 
-add_test(
-    NAME inchi_std_02
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-std-02.txt its-std-02.log NUL -AuxNone -NoLabels -NEWPSOFF
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
+function(inchi_test _name_ _args_)
+    add_test(NAME ${_name_}
+        COMMAND ${RUNNER_COMMAND} ${CMAKE_CURRENT_BINARY_DIR}/inchi-1
+            InChI_TestSet.sdf ${_name_}.txt ${_name_}.log
+            NUL -AuxNone -NoLabels ${_args_}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
+    )
+endfunction()
 
-add_test(
-    NAME inchi_std_03
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-std-03.txt its-std-03.log NUL -AuxNone -NoLabels -SNon -Key
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
+inchi_test("its-std-01" "-Key")
+inchi_test("its-std-02" "-NEWPSOFF")
+inchi_test("its-std-03" "-SNon -Key")
+inchi_test("its-std-04" "-DoNotAddH")
+inchi_test("its-std-05" "-SNon -DoNotAddH")
 
-add_test(
-    NAME inchi_std_04
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-std-04.txt its-std-04.log NUL -AuxNone -NoLabels -DoNotAddH
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_std_05
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-std-05.txt its-std-05.log NUL -AuxNone -NoLabels -SNon -DoNotAddH
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_01
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-01.txt its-non-std-01.log NUL -AuxNone -NoLabels -SUU -SLUUD
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_02
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-02.txt its-non-std-02.log NUL -AuxNone -NoLabels -SRel -Key
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_03
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-03.txt its-non-std-03.log NUL -AuxNone -NoLabels -SRac
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_04
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-04.txt its-non-std-04.log NUL -AuxNone -NoLabels -SUU -SLUUD -SUCF
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_05
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-05.txt its-non-std-05.log NUL -AuxNone -NoLabels -NEWPSOFF -SRel
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_06
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-06.txt its-non-std-06.log NUL -AuxNone -NoLabels -NEWPSOFF -SRac
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_07
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-07.txt its-non-std-07.log NUL -AuxNone -NoLabels -NEWPSOFF -SLUUD -SUCF
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_08
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-08.txt its-non-std-08.log NUL -AuxNone -NoLabels -FixedH -Key
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_09
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-09.txt its-non-std-09.log NUL -AuxNone -NoLabels -NEWPSOFF -FixedH
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_10
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-10.txt its-non-std-10.log NUL -AuxNone -NoLabels -FixedH -SNon
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_11
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-11.txt its-non-std-11.log NUL -AuxNone -NoLabels -FixedH -SRel
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_12
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-12.txt its-non-std-12.log NUL -AuxNone -NoLabels -RecMet -Key
-     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_13
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-13.txt its-non-std-13.log NUL -AuxNone -NoLabels -NEWPSOFF -RecMet
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_14
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-14.txt its-non-std-14.log NUL -AuxNone -NoLabels -RecMet -SNon
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_15
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-15.txt its-non-std-15.log NUL -AuxNone -NoLabels -RecMet -SRel
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_16
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-16.txt its-non-std-16.log NUL -AuxNone -NoLabels -FixedH -RecMet -Key
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_17
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-17.txt its-non-std-17.log NUL -AuxNone -NoLabels -NEWPSOFF -FixedH -RecMet
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_18
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-18.txt its-non-std-18.log NUL -AuxNone -NoLabels -FixedH -RecMet -SNon
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_19
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-19.txt its-non-std-19.log NUL -AuxNone -NoLabels -FixedH -RecMet -SRel
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_20
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-20.txt its-non-std-20.log NUL -AuxNone -NoLabels -KET -Key
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_21
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-21.txt its-non-std-21.log NUL -AuxNone -NoLabels -KET -SNon
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_22
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-22.txt its-non-std-22.log NUL -AuxNone -NoLabels -KET -SRel
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_23
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-23.txt its-non-std-23.log NUL -AuxNone -NoLabels -15T -Key
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_24
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-24.txt its-non-std-24.log NUL -AuxNone -NoLabels -15T -SNon
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_25
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-25.txt its-non-std-25.log NUL -AuxNone -NoLabels -15T -SRel
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_26
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-26.txt its-non-std-26.log NUL -AuxNone -NoLabels -KET -15T
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_27
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-27.txt its-non-std-27.log NUL -AuxNone -NoLabels -NEWPSOFF -KET -15T -Key
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
-
-add_test(
-    NAME inchi_nstd_28
-    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/inchi-1 InChI_TestSet.sdf its-non-std-28.txt its-non-std-28.log NUL -AuxNone -NoLabels -NEWPSOFF -KET -15T -SUU -SLUUD
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/tests
-)
+inchi_test("its-non-std-01" "-SUU -SLUUD")
+inchi_test("its-non-std-02" "-SRel -Key")
+inchi_test("its-non-std-03" "-SRac")
+inchi_test("its-non-std-04" "-SUU -SLUUD -SUCF")
+inchi_test("its-non-std-05" "-NEWPSOFF -SRel")
+inchi_test("its-non-std-06" "-NEWPSOFF -SRac")
+inchi_test("its-non-std-07" "-NEWPSOFF -SLUUD -SUCF")
+inchi_test("its-non-std-08" "-FixedH -Key")
+inchi_test("its-non-std-09" "-NEWPSOFF -FixedH")
+inchi_test("its-non-std-10" "-FixedH -SNon")
+inchi_test("its-non-std-11" "-FixedH -SRel")
+inchi_test("its-non-std-12" "-RecMet -Key")
+inchi_test("its-non-std-13" "-NEWPSOFF -RecMet")
+inchi_test("its-non-std-14" "-RecMet -SNon")
+inchi_test("its-non-std-15" "-RecMet -SRel")
+inchi_test("its-non-std-16" "-FixedH -RecMet -Key")
+inchi_test("its-non-std-17" "-NEWPSOFF -FixedH -RecMet")
+inchi_test("its-non-std-18" "-FixedH -RecMet -SNon")
+inchi_test("its-non-std-19" "-FixedH -RecMet -SRel")
+inchi_test("its-non-std-20" "-KET -Key")
+inchi_test("its-non-std-21" "-KET -SNon")
+inchi_test("its-non-std-22" "-KET -SRel")
+inchi_test("its-non-std-23" "-15T -Key")
+inchi_test("its-non-std-24" "-15T -SNon")
+inchi_test("its-non-std-25" "-15T -SRel")
+inchi_test("its-non-std-26" "-KET -15T")
+inchi_test("its-non-std-27" "-NEWPSOFF -KET -15T -Key")
+inchi_test("its-non-std-28" "-NEWPSOFF -KET -15T -SUU -SLUUD")
